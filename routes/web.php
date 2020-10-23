163880353cbf9c2559bcac2d6e58a599c6a58cd3
yu-jie shen <rock.02130529@gmail.com>
2020-10-23 21:56:02 +0800

    單元二練習一RouteSet

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return 'welcome';
+});
+
+Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return redirect('welcome');
+});
+
+
+
